b802ccaac774efb1d358747ac5bdd77cf77805e0
tim knapen <timknapen@users.noreply.github.com>
2016-10-11 01:29:44 +0200

    + add functions to the Boid

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -4,3 +4,25 @@
 //
 
 #include "Boid.hpp"
+
+//----------------------------------------------------
+Boid::Boid(){
+	speed = 0;
+}
+
+//----------------------------------------------------
+Boid::Boid(float x, float y, float dirx, float diry){
+	Boid(); // call the other costructor
+	pos.set(x,y);
+	vel.set(dirx,diry);
+	mass = 5;
+}
+
+//----------------------------------------------------
+void Boid::draw(){
+	ofPushMatrix();
+	ofTranslate(pos.x, pos.y, 0);
+	ofDrawEllipse(0, 0, 19,19);
+	ofDrawLine(0, 0, 10*vel.x, 10*vel.y);
+	ofPopMatrix();
+}
--- src/Boid.hpp
+++ src/Boid.hpp
@@ -11,8 +11,23 @@
 class Boid{
 
 public:// All attributes and functions here can be called by other classes
+	
+	// The constructor
+	Boid();
+	Boid(float x, float y, float dirx, float diry); // Another constructor!
+	
+	void draw(); // draw the Boid to screen
+	
+	ofPoint pos; // my position vector
+	ofPoint vel; // my velocity vector
+	ofPoint acc; // my acceleration vector ( F = m * a )
+	
+	float speed;	
+	float mass;
+	
 private:
 	// all attributes and functions here can only be called by the instance/class itself
+	
 };
 
 #endif /* Boid_hpp */
